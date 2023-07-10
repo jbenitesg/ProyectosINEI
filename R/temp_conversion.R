2743d15f80c708b1966ad54c110e754107dfe6a6
jbenitesg <jbenitesg@pucp.edu.pe>
2023-07-10 16:33:51 -0500

    update
    consulta_endes

--- R/temp_conversion.R
+++ R/temp_conversion.R
@@ -6,7 +6,7 @@
 #' @param base Nombre de la base a consultar
 #' @return Base del módulo de interés de la ENAHO
 #' @examples
-#' temp1 <- consulta_enaho(periodo = 2020, codigo_modulo = 1, base = "enaho01-100-2022")
+#' temp1 <- consulta_enaho(periodo = 2020, codigo_modulo = 01, base = "enaho01-100-2022")
 #' @export
 consulta_enaho <- function(periodo,
                            codigo_modulo,
@@ -75,6 +75,87 @@ consulta_enaho <- function(periodo,
     data
   }
 }
+
+
+
+#' Encuesta Demográfica y de Salud Familiar - ENDES
+#'
+#' Consulta las bases de datos de la ENDES de forma directa
+#' @param periodo Año de consulta
+#' @param codigo_modulo ID del módulo (Dos dígitos para la ENDES)
+#' @param base Nombre de la base a consultar
+#' @return Base del módulo de interés de la ENDES
+#' @examples
+#' temp1 <- consulta_endes(periodo = 2004, codigo_modulo = 64, base = "RECH0")
+#' @export
+consulta_endes <- function(periodo,
+                           codigo_modulo,
+                           base,
+                           guardar = FALSE,
+                           ruta = "",
+                           codificacion = NULL) {
+  # Generamos dos objetos temporales: un archivo y una carpeta
+  temp <- tempfile() ; tempdir <- tempdir()
+
+  # Genera una matriz con el número identificador de versiones por cada año
+  versiones <- matrix(
+    c(
+      2022, 786,
+      2021, 760,
+      2020, 739,
+      2019, 691,
+      2018, 638,
+      2017, 605,
+      2016, 548,
+      2015, 504,
+      2014, 441,
+      2013, 407,
+      2012, 323,
+      2011, 290,
+      2010, 260,
+      2009, 238,
+      2008, 209,
+      2007, 194,
+      2006, 183,
+      2005, 150,
+      2004, 120),
+    byrow = T,
+    ncol = 2)
+
+  # Extrae el código de la encuesta con la matriz versiones
+  codigo_encuesta <- versiones[versiones[,1] == periodo,2]
+  ruta_base <- "https://proyectos.inei.gob.pe/iinei/srienaho/descarga/SPSS/" # La ruta de microdatos INEI
+  modulo <- glue("-Modulo{codigo_modulo}.zip")
+  url <- glue("{ruta_base}{codigo_encuesta}{modulo}")
+  #https://proyectos.inei.gob.pe/iinei/srienaho/descarga/SPSS/548-Modulo64.zip
+
+  # Descargamos el archivo
+  utils::download.file(url,temp, timeout=120)
+
+  # Listamos los archivos descargados y seleccionamos la base elegida
+  archivos <- utils::unzip(temp,list = T)
+  archivos <- archivos[stringr::str_detect(archivos$Name, paste0(base,"\\.")) == TRUE,]
+
+  # Elegimos entre guardar los archivos o pasarlos directamente a un objeto
+  if(guardar == TRUE) {
+    utils::unzip(temp, files = archivos$Name, exdir = paste(getwd(), "/", ruta, sep = ""))
+    print(paste("Archivos descargados en: ", getwd(), "/", ruta, sep = ""))
+  }
+  else {
+    data <- haven::read_sav(
+      utils::unzip(
+        temp,
+        files = archivos$Name[grepl(".sav|.SAV",archivos$Name)],
+        exdir = tempdir
+      ),
+      encoding = codificacion
+    )
+    nombres <- tolower(colnames(data))
+    colnames(data) <- nombres
+    data
+  }
+}
+
 
 #' Encuesta Nacional de Programas Presupuestales
 #'
